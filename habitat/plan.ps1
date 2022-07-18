f68c66f525fac35ff5170d1f358f1b8e187ccdae
John McCrae <john.mccrae@progress.com>
2022-07-18 15:53:07 -0700

    starting to integrate ruby 3.1 into the builds
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -66,9 +66,10 @@ function Invoke-Prepare {
         Write-BuildLine " ** Where the hell is 'Gem'?"
         $gems = Get-ChildItem -path . -File "gem.bat"
         Write-BuildLine "*** Send in the Gems!"
-        foreach($gem in $gems){
-            $gem.FullName
-        }
+        Write-BuildLine $gems
+        # foreach($gem in $gems){
+        #     $gem.FullName
+        # }
         $source_path = Get-ChildItem -path "C:\hab\studios" -File "gem.cmd" -Recurse -ErrorAction SilentlyContinue
         $end_path = (Get-Item $source_path.DirectoryName).Parent.FullName
         $bin_path = (Get-Item $source_path.DirectoryName).FullName
